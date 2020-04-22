4ef1b47009be4a9fe8340462d481fe04df309e87
PergerTech <jwaltisperger@gmail.com>
2020-04-22 11:10:44 -0500

    '.'

--- SolarPV/admin.py
+++ SolarPV/admin.py
@@ -12,6 +12,7 @@ admin.site.register(Profile, ProfileAdmin)
 
 # Manufacturer admin view
 class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'contact_person', 'registered_country')
     fields = ('company_name', 'contact_person', 'registered_country')
     list_filter = ('company_name', 'manufacturer_id', 'registered_country')
 
